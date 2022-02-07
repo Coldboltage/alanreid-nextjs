32447d0b61402fd03aa1f29621be57a6d2205f52
Coldboltage <alanreid@hotmail.co.uk>
2022-02-07 17:53:54 +0000

    correcting background colours and container styles

--- components/Footer.js
+++ components/Footer.js
@@ -3,18 +3,17 @@ import Image from "next/image"
 
 const Footer = () => {
   return (
-    <footer className="">
-      //{" "}
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <span className="">
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
+    <footer className="flex justify-between p-2">
+      <div>
+        <p className="text-xs">&copy; Alan Reid Portfolio {new Date().getFullYear()}</p>
+      </div>
+      <div>
+        <ul className="flex justify-between space-x-2 text-xs">
+          <li>Home</li>
+          <li>Blog</li>
+          <li>Projects</li>
+        </ul>
+      </div>
     </footer>
   );
 };
--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   // Header for each page
-  return <header>
-    {/* Nav for the top of each page */}
-    {/* >> Each Section  */}
-    <nav className="grid grid-cols-2 p-4 space-between bg-black">
-      {/* Divided into two section */}
-      <section className="text-white">
-        Alan Reid
-      </section>
-      <section className="grid grid-cols-2 text-white text-center">
-        <div>Blog</div>
-        <div>Projects</div>
-      </section>
-    </nav>
-  </header>;
+  return (
+    <header className="bg-black">
+      <div className="container mx-auto">
+        {/* Nav for the top of each page */}
+        {/* >> Each Section  */}
+        <nav className="grid grid-cols-2 p-4 space-between">
+          {/* Divided into two section */}
+          <section className="text-white">Alan Reid</section>
+          <section className="grid grid-cols-2 text-center text-white">
+            <div>Blog</div>
+            <div>Projects</div>
+          </section>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
