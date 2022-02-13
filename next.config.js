4289511b59e04f39ea70f11bf78d57b2fbc1e524
Coldboltage <alanreid@hotmail.co.uk>
2022-02-13 14:49:59 +0000

    added next back

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-})
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-})
-
-// module.exports = {
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
-// }
+module.exports = {
+  images: {
+    formats: ['image/avif', 'image/webp']
+  }
+}
