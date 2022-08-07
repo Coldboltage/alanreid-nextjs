337fc8ecc31adcca6bdd6a0a86f5f02301ad270e
Coldboltage <alanreid@hotmail.co.uk>
2022-08-07 21:04:44 +0100

    added new skills

--- constants/skills.js
+++ constants/skills.js
@@ -1,17 +1,17 @@
 export default [
   {
     icon: "/assets/icons/gatsby.png",
-    name: "GatsbyJS",
+    name: "Gatsby",
     description: "as my frontend framework for non dynamic needs"
   },
   {
     icon: "/assets/icons/next.png",
-    name: "NextJS",
+    name: "Next",
     description: "as my frontend framework for dynamic scalable needs"
   },
   {
     icon: "/assets/icons/tailwind.png",
-    name: "Tailwind CSS",
+    name: "Tailwind",
     description: "as my CSS Framework"
   },
   {
@@ -21,23 +21,33 @@ export default [
   },
   {
     icon: "/assets/icons/meteor.png",
-    name: "MeteorJS",
+    name: "Meteor",
     description: "when I need to build minimal vaiable products"
   },
   {
     icon: "/assets/icons/node.png",
-    name: "NodeJS",
+    name: "Node",
     description: "as my general backend JS runtime"
   },
   {
     icon: "/assets/icons/express.png",
-    name: "ExpressJS",
+    name: "Express",
     description: "for building APIs and webservers "
   },
   {
     icon: "/assets/icons/puppeteer.png",
     name: "Puppeteer",
     description: "for web automation and scraping data"
+  },
+  {
+    icon: "/assets/icons/jest.png",
+    name: "Jest",
+    description: "for Test Driven Development / Testing"
+  },
+  {
+    icon: "/assets/icons/cucumber.png",
+    name: "Cucumber",
+    description: "for Behavioural Driven Development / Testing"
   },
   {
     icon: "/assets/icons/google.png",
