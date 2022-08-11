21351afafb834fd2fa6634544efc6f3314a528dc
Coldboltage <alanreid@hotmail.co.uk>
2022-08-11 22:00:38 +0100

    fixed bug

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -12,9 +13,17 @@ const Footer = () => {
         </div>
         <div>
           <ul className="flex-row justify-between text-xs text-center sm:flex sm:space-x-3">
-            <li>Home</li>
-            <li>Blog</li>
-            <li>Projects</li>
+            <Link href="/about">About</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/cv">CV</Link>
+            <Link href="/projects">Projects</Link>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/coldboltage"
+            >
+              GitHub
+            </a>
           </ul>
         </div>
       </footer>
