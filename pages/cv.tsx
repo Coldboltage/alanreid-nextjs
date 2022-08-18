44c5e12761e96394a98f0e77c98ea2526f9bd49b
unknown <alandreid@hotmail.co.uk>
2022-08-18 11:40:36 +0100

    edited styling to work on a nested div bases rather than full parent div

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -10,9 +10,9 @@ const CV: NextPage = () => {
   return (
     <Layout>
       <div className="bg-[#191B1F] text-white pb-10">
-        <div className="container-layout sm:px-36">
+        <div className="sm:px-36">
           {/* Title */}
-          <div className="flex flex-col gap-2 pt-10 mx-auto sm:gap-4 sm:pt-16 ">
+          <div className="container-layout sm:px-36 flex flex-col gap-2 pt-10 mx-auto sm:gap-4 sm:pt-16 ">
             <h1 className="text-3xl sm:text-5xl font-semibold underline-offset-4 underline decoration-[#26a8ed]">
               CV
             </h1>
@@ -20,27 +20,32 @@ const CV: NextPage = () => {
           </div>
           {/* Picture */}
           {/* Information */}
-          <div className="py-10 text-lg">
+          <div className="container-layout sm:px-36 py-10 text-lg">
             <p className="text-base leading-8">
               {`I build solutions for businesses which meet the highest of SEO standards. This is achieved using Gatsby or Next for the frontend. For backend work, I love using Node and Express with Puppeteer.`}
             </p>
           </div>
-          <div className="pb-10 text-lg">
+          <div className="container-layout sm:px-36 pb-10 text-lg">
             <h2 className="pb-5 text-xl font-bold sm:text-2xl">
               My Javascript Programming Stack
             </h2>
             <div className="flex flex-col gap-2 pt-5 text-sm">
               {skills.map((skill) => (
                 <div key={skill.name} className="flex gap-4 border-b-[1px] pb-2 items-center">
-                  <Image src={skill.icon} width={40} height={40} alt={`${skill.name}'s icon`}/>
+                  <Image src={skill.icon} width={40} height={40} alt={`${skill.name}'s icon`} />
                   <p><span className="italic font-bold">{skill.name}</span>{` ${skill.description}`}</p>
                 </div>
               ))}
             </div>
           </div>
-          <p className="pb-5 text-base">
-           {` With 10 years of SEO experience, I'm able to shape marketing departments and help them to become more SEO aware. `}
+          <p className="container-layout sm:px-36  pb-5 text-base">
+            {` With 10 years of SEO experience, I'm able to shape marketing departments and help them to become more SEO aware. `}
           </p>
+          <div className="container-layout sm:px-36 pb-10 text-lg">
+            <h2 className="pb-5 text-xl font-bold sm:text-2xl">
+              Courses I've taken
+            </h2>
+          </div>
         </div>
       </div>
       <CallToAction />
